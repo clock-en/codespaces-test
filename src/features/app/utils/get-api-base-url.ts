6864df9c3aeaca364cea0e90ad3c65ec1e104f1d
clock-en <k.kuroda.biz@gmail.com>
2023-12-06 21:12:52 +0900

    環境変数名を変更

--- src/features/app/utils/get-api-base-url.ts
+++ src/features/app/utils/get-api-base-url.ts
@@ -1,6 +1,6 @@
 export const getApiBaseUrl = () => {
   if (process.env.CODESPACE_NAME) return `https://${process.env.CODESPACE_NAME}-8000.app.github.dev`;
-  if (process.env.API_URL) return process.env.API_URL;
+  if (process.env.NEXT_PUBLIC_API_URL) return process.env.NEXT_PUBLIC_API_URL;
 
   return 'http://localhost:8000';
 };
